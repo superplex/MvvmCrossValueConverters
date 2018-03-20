cdf1f3d3164729c5d388fb208b8924819c0a81b3
superplex <superplex2018@gmail.com>
2018-03-20 13:16:17 +0000

    Initial commit

---
+++ MvvmCrossValueConverters/ValueConverters/BoolNegationValueConverter.cs
@@ -0,0 +1,17 @@
+﻿using MvvmCross.Platform.Converters;
+
+namespace MvvmCrossValueConverters.ValueConverters
+{
+    public class BoolNegationValueConverter : MvxValueConverter<bool, bool>
+    {
+        protected override bool Convert(bool value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !value;
+        }
+
+        protected override bool ConvertBack(bool value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !value;
+        }
+    }
+}
